2cd642cbab6fa9b83c0d60b62cf5d3e10d38eb18
therealmensumit <sk03950@gmail.com>
2022-04-08 10:49:06 +0530

    some changes

--- bannerform.php
+++ bannerform.php
@@ -47,7 +47,35 @@
             </div>
         </form>
     </div>
+    <script src="./js/intlTelInput.js" type="text/javascript"></script>
+    <script src="./js/utils.js"></script>
     <script src="js/script.js"></script>
+    <script>
+        var baninput = document.querySelector(".bphone");
+            window.intlTelInput(baninput, {
+                // allowDropdown: false,
+                // autoHideDialCode: false,
+                autoPlaceholder: "off",
+                // dropdownContainer: document.body,
+                // excludeCountries: ["us"],
+                //formatOnDisplay: false,
+                // geoIpLookup: function(callback) {
+                //   $.get("http://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+                //     var countryCode = (resp && resp.country) ? resp.country : "";
+                //     callback(countryCode);
+                //   });
+                // },
+                // hiddenInput: "full_number",
+                initialCountry: "in",
+                // localizedCountries: { 'de': 'Deutschland' },
+                // nationalMode: false,
+                // onlyCountries: ['us', 'gb', 'ch', 'ca', 'do'],
+                // placeholderNumberType: "MOBILE",
+                // preferredCountries: ['cn', 'jp'],
+                //separateDialCode: true,
+                utilsScript: "js/utils.js",
+            });
+    </script>
 </body>
 
 </html>
